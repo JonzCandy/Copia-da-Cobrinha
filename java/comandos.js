bc84d22b9c71db5a79c0d05e70e90d5e716187c8
programer-jon <joncandy2021@gmail.com>
2021-07-09 11:34:46 -0300

    criando o cachorro

--- java/comandos.js
+++ java/comandos.js
@@ -1,10 +1,23 @@
 let canvas = document.getElementById("cachorro");
 let context = canvas.getContext("2d");
 let box = 32;
+let cachorro = [];
+cachorro[0] = {
+    x: 8 * box,
+    y: 8 * box
+}
 
 function criarBG() {
     context.fillStyle = "Lightgray";
     context.fillRect(0, 0, 16 * box, 16 * box)
 }
 
+function criarCachorro() {
+    for (i = 0; i < cachorro.length; i++) {
+        context.fillStyle = "yellow";
+        context.fillRect(cachorro[i].x, cachorro[i].y, box, box);
+    }
+}
+
 criarBG();
+criarCachorro();
